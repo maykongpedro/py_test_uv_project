61d497e0f3f52ced5923710f0880310d8e9a9c84
maykongpedro <54289045+maykongpedro@users.noreply.github.com>
2025-11-10 00:28:14 -0300

    add pyomo problems

--- polars_test_gapminder.py
+++ polars_test_gapminder.py
@@ -1,30 +1,25 @@
-
-# import librarys
+# %% Import librarys ==============================================================
 import polars as pl
 import plotly.express as px
 import pyarrow
 
-# import data
+# %% Import data ==============================================================
 gapminder = pl.read_parquet("gapminder_data.parquet")
 
-# see data structure
+# %% See data structure ==============================================================
 gapminder.dtypes
 gapminder.schema
 
-# fix types of columns
-gapminder_ft = (
-  gapminder
-  .with_columns([
-    # from caegorical to utf8 (string)
-    pl.col('country').cast(pl.Utf8),
-    pl.col('continent').cast(pl.Utf8),
-    pl.col('region').cast(pl.Utf8)
-  ])
+# %% Fix column types ==============================================================
+gapminder_ft = gapminder.with_columns(
+    [
+        # from caegorical to utf8 (string)
+        pl.col("country").cast(pl.Utf8),
+        pl.col("continent").cast(pl.Utf8),
+        pl.col("region").cast(pl.Utf8),
+    ]
 )
 
-# check structure
+# %% Check structure and see first rows ===============================================
 gapminder_ft.schema
-
-# see first 10 rows
 gapminder_ft.head(10)
-
